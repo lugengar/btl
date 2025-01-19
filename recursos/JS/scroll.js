4cf189d0cefad12922ca35814999a0b1b2140731
lugengar <luciano.barbinii@gmail.com>
2025-01-19 20:49:45 -0300

    a

--- recursos/JS/scroll.js
+++ recursos/JS/scroll.js
@@ -1,9 +1,9 @@
-function isElementInViewport(el) {
+function isElementInViewport(el, offset = 100) { 
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
+        rect.top >= -offset &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.bottom <= (window.innerHeight + offset || document.documentElement.clientHeight + offset) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
@@ -11,12 +11,11 @@ function isElementInViewport(el) {
 function handleScroll() {
     const elements = document.querySelectorAll('.listaproductos, .listaservicios, .titulo, .inputs, #producto h1, #producto p, .botonegro, .minitexto, #servicios .titulo');
     elements.forEach((element) => {
-        if (isElementInViewport(element)) {
+        if (isElementInViewport(element, 200)) { 
             element.classList.add('aparece');
         }
     });
 }
 
 window.addEventListener('scroll', handleScroll);
-
 document.addEventListener('DOMContentLoaded', handleScroll);
