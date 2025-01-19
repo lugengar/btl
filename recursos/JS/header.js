a4e3e86c4e183369fb84c83f5f3cf71b30cdcb97
lugengar <luciano.barbinii@gmail.com>
2025-01-18 21:08:12 -0300

    a

--- recursos/JS/header.js
+++ recursos/JS/header.js
@@ -2,14 +2,21 @@ let bar =false
 
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
+    var nosotros = document.getElementById('nosotros');
     var scrollPosition = window.scrollY || window.pageYOffset;
 
-    var triggerPoint = 100; 
 
-    if (scrollPosition >= triggerPoint) {
+    if (scrollPosition >= 100) {
         header.classList.add('hidden');
         header.classList.remove('visible');
+    }
+    if (scrollPosition >= 500) {
+        nosotros.style.opacity = "100%";
+        nosotros.style.pointerEvents= "all";
     } else {
+        nosotros.style.opacity = "0%";
+        nosotros.stylepointerEvents= "none";
+        
         header.classList.add('visible');
         header.classList.remove('hidden');
     }
