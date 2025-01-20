9f9252e45a0857aa6dcf035fc9abb5b3399a2eae
lugengar <luciano.barbinii@gmail.com>
2025-01-19 22:10:34 -0300

    a

---
+++ recursos/JS/video.js
@@ -0,0 +1,11 @@
+
+fetch('recursos/JSON/video.json') 
+
+    .then(response => response.json())
+    .then(data => {
+        const iframe = document.getElementById('iframe');
+        
+            iframe.src = data.url
+    })
+    .catch(error => console.error('Error cargando el JSON:', error));
+
