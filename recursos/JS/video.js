eff9a26f77d8ed6e4390e6b6bf98a4b798473180
lugengar <luciano.barbinii@gmail.com>
2025-01-21 13:11:49 -0300

    a

--- recursos/JS/video.js
+++ recursos/JS/video.js
@@ -1,10 +1,15 @@
+//https://www.youtube.com/embed/y47Htq3Ol2c?si=cg-ouPmtVw1mbZg0&amp;controls=0
 
 fetch('recursos/JSON/video.json') 
 
     .then(response => response.json())
     .then(data => {
         const iframe = document.getElementById('iframe');
-        iframe.src = data.url
+        if(data.url == ""){
+            iframe.parentNode.style.display="none"
+        }else{
+            iframe.src = data.url
+        }
     })
     .catch(error => console.error('Error cargando el JSON:', error));
 
